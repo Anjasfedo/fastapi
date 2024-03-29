06da89a36ef39303ef973e8d8e8faa18911c6a14
AnjasFedo <fedoaf98@gmail.com>
2024-03-30 06:21:44 +0700

    HTTP Requests

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.params import Body
 
 app = FastAPI()
 
@@ -7,6 +8,13 @@ app = FastAPI()
 def root():
     return {"message": "Hewroo worldo"}
 
+
 @app.get("/posts")
 def get_posts():
     return {"data": "Yer post"}
+
+
+@app.post("/createposts")
+def create_post(payLoad: dict = Body(...)):
+    print(payLoad)
+    return {"new_post": payLoad}
