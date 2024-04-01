56724dcd2ba8488479b815bec77c0fdb798cb6d7
AnjasFedo <fedoaf98@gmail.com>
2024-04-01 22:39:09 +0700

    OAuth2 PasswordRequestForm

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -5,14 +5,15 @@ from ..schemas import UserLogin
 from .. import models
 from ..utils import verify_password
 from ..oauth2 import create_access_token
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 
 router = APIRouter(tags=["Authentication"])
 
 
 @router.post("/login")
-def login(user_crendentials: UserLogin, db: Session = Depends(connect_db)):
+def login(user_crendentials: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(connect_db)):
     user = db.query(models.User).filter(models.User.email ==
-                                        user_crendentials.email).one_or_none()
+                                        user_crendentials.username).one_or_none()
 
     if user == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
@@ -21,7 +22,7 @@ def login(user_crendentials: UserLogin, db: Session = Depends(connect_db)):
     if not verify_password(user_crendentials.password, user.password):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"Invalid Credentials")
-        
+
     access_token = create_access_token(data={"user_id": user.id})
 
     return {"access_token": access_token, "token_type": "bearer"}
